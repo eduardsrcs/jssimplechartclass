5bc6b282e73689341be4621dac4717c129c24101
eduardsz <eduards.zvaigzne@gmail.com>
2021-03-30 15:17:43 +0300

    created changeChart

--- js/proop.js
+++ js/proop.js
@@ -2,7 +2,6 @@ import {Chart} from './mychart.js'
 import {progresses} from './pcharts.js'
 
 const element = document.getElementById('progresses')
-
 const chart = new Chart(element)
 
 progresses.forEach(element => {
@@ -12,3 +11,11 @@ progresses.forEach(element => {
 setTimeout(() => {
   chart.changeChart('Hello', 70)
 }, 1000)
+
+setTimeout(() => {
+  chart.changeChart('Vue.js', 40)
+}, 2000)
+
+setTimeout(() => {
+  chart.changeChart('Laravel 8 new shop', 14)
+}, 4000)
