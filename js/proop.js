8ad31186b2fca2c585efde8741de0c1a9cb4d055
eduardsz <eduards.zvaigzne@gmail.com>
2021-04-03 16:01:12 +0300

    added theme switch

--- js/proop.js
+++ js/proop.js
@@ -1,10 +1,24 @@
 import {Chart} from './mychart.js'
 import {progresses} from './pcharts.js'
 
+
 const element = document.getElementById('progresses')
 const chart = new Chart(element)
 
 progresses.forEach(element => {
   chart.addChart(element)
 });
+
+if(localStorage.getItem('night')) document.body.classList.add('night')
+
+nightthemesw.addEventListener('click', () => {
+  console.log('click');
+  if(localStorage.getItem('night')) {
+    document.body.classList.remove('night')
+    localStorage.removeItem('night')
+  } else {
+    document.body.classList.add('night')
+    localStorage.setItem('night', true)
+  }
+})
 
