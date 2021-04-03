9250a026f7e78b8ff0d074552db24b62eee2bab1
eduardsz <eduards.zvaigzne@gmail.com>
2021-04-03 15:44:04 +0300

    added night theme

--- js/pcharts.js
+++ js/pcharts.js
@@ -1,6 +1,8 @@
 export const progresses = [
-  {title: '', name: 'Hello', value: 60, color: '#cce', desc: 'Just say hello'},
-  {title: 'Node', name: 'Cобеседование Junior NODE JS разработчика', value: 15, color: '#ece', desc: 'Paused at <a href="https://www.youtube.com/watch?v=2DIBCDxYkUc&t=795s">13:15</a>, path: js/jobinterview'},
+  {title: '<b>Title</b>', name: '<b>Name</b>', value: 'Progress, ', color: 'rgba(0, 100, 100, .7)', desc: '<b>Description</b>'},
+  {title: 'Node', name: 'Cобеседование Junior NODE JS разработчика', value: 15, color: '#ece', desc: 'Paused at <a href="https://www.youtube.com/watch?v=2DIBCDxYkUc&t=795s">13:15</a>, path: jobinterview/ji1_node.md'},
   {title: 'CI/CD', name: 'Просто о CI/CD (Непрерывная интеграция и доставка)', value: 1, color: '#e4e', desc: 'Paused at <a href="https://www.youtube.com/watch?v=7S1ndRRht6M">0:00</a>, path: no'},
+  {title: 'Minin', name: 'Canvas графики для Telegram | Мастер-класс', value: 2, color: '#94e', desc: 'Paused at <a href="https://www.youtube.com/watch?v=yz09oVHt3PY&t=3165s">52:45</a>, path: minin/canvas_charts'},
+  {title: 'Диджитализируй!', name: 'Делаем ПРАВИЛЬНУЮ маску ввода телефона.', value: 3, color: '#94e', desc: 'Paused at <a href="https://www.youtube.com/watch?v=Lxj_v5z0xRE&t=720s">20:00</a>, path: js/phoneinput'},
 
 ]
--- js/proop.js
+++ js/proop.js
@@ -8,14 +8,3 @@ progresses.forEach(element => {
   chart.addChart(element)
 });
 
-setTimeout(() => {
-  chart.changeChart('Hello', 70)
-}, 1000)
-
-setTimeout(() => {
-  chart.changeChart('Vue.js', 40)
-}, 2000)
-
-setTimeout(() => {
-  chart.changeChart('Laravel 8 new shop', 14)
-}, 4000)
